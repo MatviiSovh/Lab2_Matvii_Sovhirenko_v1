85df3990e3335da8157917be94b7459e58ea0ddc
matvii <matveysovh@gmail.com>
2023-12-11 11:55:40 +0200

    Revert "testing"
    This reverts commit 515300baa78ac274bf651d2a65fc63fb66d1b60d.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.sonarqube") version "3.3"
 }
 
 android {
@@ -30,49 +29,11 @@ android {
 }
 
 dependencies {
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-
-    androidTestImplementation("org.mockito:mockito-core:3.12.4")
-    androidTestImplementation("org.mockito:mockito-android:3.12.4")
-
-    implementation("fileTree(dir: 'libs', include: ['*.jar'])")
-    implementation("androidx.appcompat:appcompat:1.0.2")
-    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
-
-    testImplementation("junit:junit:4.12")
-    testImplementation("project(path: ':app')")
-    androidTestImplementation("androidx.test:runner:1.1.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.0-alpha01")
-
-    androidTestImplementation("com.android.support.test:rules:1.0.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.0-alpha01")
-
 }
-
-sonarqube {
-    properties {
-        property("sonar.projectName", "TestingExample")
-        property("sonar.projectKey", "TestingExample")
-        property("sonar.sourceEncoding", "UTF-8")
-        property("sonar.sources", "src/main/java")
-        property("sonar.exclusions",
-                """
-            **/*Test*/**,
-            *.json,
-            **/*test*/**,
-            **/.gradle/**,
-            **/R.class
-            """
-        )
-        property("sonar.tests", "src/test/java")
-    }
-}
-
-
-
